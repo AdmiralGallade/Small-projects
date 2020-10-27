460bd4ba3f40f34e79cc1984e2eec79b95f07a38
AdmiralGallade <mailforkushagra2@gmail.com>
2020-10-27 07:59:14 +0530

    Update irelia.py

--- others/irelia.py
+++ others/irelia.py
@@ -1,16 +1,21 @@
 from math import floor
 import matplotlib.pyplot as plt
 import numpy as np
-x=2000
 
-# for i in range(1,10):
-#     x=x-0.12*x
-#     print("Attack ", i," reduced health to " , floor(x))
+#calculates damage by blade of the ruined king passive.
+x=[2000]
+
+for i in range(0,20):
+
+    z=x[i]-0.12*x[i]
+    int(floor(z))
+    x.append(z)
+# print (x)
 
 
 x_axis= np.arange(0,2000,200).tolist()
 
-y_axis= list(range(1,11,1))
+y_axis= list(range(0,21,1))
 y_axis.reverse()
 
 print( x_axis,"\n",y_axis)
@@ -19,3 +24,8 @@ print( x_axis,"\n",y_axis)
 plt.plot(x_axis,y_axis)
 plt.figure()
 
+
+plt.xlabel("Number of physical attacks by blade of ruined king")
+plt.ylabel("Health of enemy marksman")
+
+plt.plot(y_axis,x)
